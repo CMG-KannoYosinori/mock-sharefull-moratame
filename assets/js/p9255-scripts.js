0a2a83498a403a19bf5dc426610a8a2a223c2ba4
Yoshinori Kanno <yoshinori.kanno@me.com>
2026-03-12 23:48:33 +0900

    Updates

--- assets/js/p9255-scripts.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-{
-  document
-    .querySelector(".p9255-sticky-button")
-    ?.addEventListener("click", () => {
-      document.querySelector(".p9255-sticky-button").style.display = "none"
-    })
-}
