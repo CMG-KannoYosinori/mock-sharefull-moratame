3e348c206e0ec8854517bdce111d2ccd8d1117b6
Yoshinori Kanno <yoshinori.kanno@me.com>
2026-03-13 00:37:13 +0900

    🔄 JavaScriptのコードを整形し、HTMLファイル内のリンクを相対パスに変更。これにより、可読性とメンテナンス性が向上し、全体のパフォーマンスが改善される。また、CSSスタイルを追加し、メニューのリンク色を変更。

--- legacy/moratame/s/js/pagetop.js
+++ legacy/moratame/s/js/pagetop.js
@@ -1,20 +1,21 @@
+
 var nowPosition = 0;
 
-$(function() {	
-$('#scrollContainer').click(function () {
-$('body,html').scrollTop(0);
-});
+$(function () {
+  $('#scrollContainer').click(function () {
+    $('body,html').scrollTop(0);
+  });
 });
 
-$(function() {	
-$('#scrollContainer').hide();
-$(window).scroll(function(){
-diffPosition = nowPosition - $(window).scrollTop();
-nowPosition = $(window).scrollTop();
-if(diffPosition <= 0 || nowPosition <= 50 ){
-	$('#scrollContainer').stop(false, true).fadeOut();
-}else{
-$('#scrollContainer').stop(false, true).fadeIn();
-}
-});
+$(function () {
+  $('#scrollContainer').hide();
+  $(window).scroll(function () {
+    diffPosition = nowPosition - $(window).scrollTop();
+    nowPosition = $(window).scrollTop();
+    if (diffPosition <= 0 || nowPosition <= 50) {
+      $('#scrollContainer').stop(false, true).fadeOut();
+    } else {
+      $('#scrollContainer').stop(false, true).fadeIn();
+    }
+  });
 });
