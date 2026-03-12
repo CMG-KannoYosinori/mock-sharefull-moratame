2dce58ccf5e21d59041622e34b341513d813d7e0
Yoshinori Kanno <yoshinori.kanno@me.com>
2026-03-12 17:46:02 +0900

    Updates

--- assets/js/jquery.mobile.custom.js
+++
@@ -1,8 +0,0 @@
-
-// Change AJAX LINK
-$(document).bind("mobileinit", function(){
-    $.mobile.metaViewportContent = "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=1";
-    $.mobile.ajaxLinksEnabled = true;
-    $.mobile.ajaxFormsEnabled = false;
-});
-
