cce289d127d9d690e26719c91957e24061264826
Yoshinori Kanno <yoshinori.kanno@me.com>
2026-03-12 19:28:01 +0900

    🔄 不要なJavaScriptおよびCSSファイルを削除し、関連するHTMLファイルからのリンクを整理。これにより、コードベースの軽量化とメンテナンス性の向上が図られる。また、SMS認証関連のHTML構造を改善し、ユーザー体験を向上させるための調整を実施。

--- src/assets/js/announce_toggle.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $('#announceTgl').click(function () {
-    $('#announce').slideToggle();
-  });
-  $('#announce').bind('mouseleave', function () {
-    $('#announce').slideUp();
-  });
-}); announce_toggle.j
