99b045e3697c75d6bd3491da448c42b8efa8747d
Yoshinori Kanno <yoshinori.kanno@me.com>
2026-03-19 17:54:35 +0900

    Updates

--- assets/js/t.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-  $(".js-t-accordion").on("click", function () {
-    var $this = $(this);
-    $this.next().slideToggle("normal");
-    $this.toggleClass("t-arrow_down t-arrow_up");
-  });
-}(jQuery));
